7169d03d9b3e420ad0165b92aaabd11acd9fa3a8
Viraj-Kaushik <kaushikviraj5@gmail.com>
2025-05-25 20:01:12 +0530

    Fix Vercel deployment by using stable TailwindCSS v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
